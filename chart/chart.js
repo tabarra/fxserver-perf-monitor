4a94d061502a28eaa931aecfd3f3301557404adf
Andre Tabarra <tabarra@vexguard.net>
2021-01-03 18:51:38 -0300

    added client count to chart

--- chart/chart.js
+++ chart/chart.js
@@ -12,7 +12,7 @@ const drawHeatmap = (d3Container, perfData) => {
     const width = d3Container.offsetWidth;
     const timeTickInterval = 15;
     const height = 340;
-    const margin = { top: 0, right: 0, bottom: 20, left: 75 };
+    const margin = { top: 10, right: 55, bottom: 20, left: 75 };
     const color = d3.scaleSequential(d3.interpolateInferno)
         .domain([0, 1])
 
@@ -25,7 +25,10 @@ const drawHeatmap = (d3Container, perfData) => {
     for (let snapIndex = 0; snapIndex < perfData.length; snapIndex++) {
         const snap = perfData[snapIndex];
         snapAvgTickTimes.push(snap.avgTime);
-        snapClients.push(snap.clients);
+        snapClients.push({
+            x: snapIndex,
+            c: snap.clients
+        });
 
         //Process times
         const time = new Date(snap.ts);
@@ -33,8 +36,8 @@ const drawHeatmap = (d3Container, perfData) => {
             const hours = String(time.getHours()).padStart(2, "0");
             const minutes = String(time.getMinutes()).padStart(2, "0");
             snapTimes.push({
-                x: snapIndex, 
-                t: `${hours}:${minutes}` 
+                x: snapIndex,
+                t: `${hours}:${minutes}`
             })
         }
 
@@ -50,34 +53,29 @@ const drawHeatmap = (d3Container, perfData) => {
     }
     // console.log(snapTimes)
 
-
+    //Creating SVG
     const svg = d3.create("svg")
         .attr("viewBox", [0, 0, width, height]);
 
     // X Axis - Time
-    const x = d3.scaleBand()
+    const timeScale = d3.scaleBand()
         .domain(d3.range(perfData.length))
         .range([margin.left, width - margin.right])
-    const xAxis = d3.axisBottom(x)
+    const timeAxis = d3.axisBottom(timeScale)
         .tickValues(snapTimes.map(t => t.x))
-        .tickFormat((d, i) => {
-            console.log(d, i, snapTimes[i].t)
-            return snapTimes[i].t
-        })
-
+        .tickFormat((d, i) => snapTimes[i].t)
     svg.append('g')
         .attr("transform", translate(0, height - margin.bottom))
         .attr("id", "x-axis")
         .attr("class", "axis")
-        .call(xAxis);
+        .call(timeAxis);
 
 
     // Y Axis - Tick Times
-    const y = d3.scaleBand()
+    const tickBucketsScale = d3.scaleBand()
         .domain(d3.range(yLabels.length))
         .range([height - margin.bottom, margin.top])
-
-    const yTitle = g => g.append("text")
+    const tickBucketsTitle = g => g.append("text")
         .attr("transform", "rotate(-90)")
         .attr("class", "axisTitle")
         .attr("y", 0)
@@ -85,15 +83,14 @@ const drawHeatmap = (d3Container, perfData) => {
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Tick Time (lower is better)")
-    svg.call(yTitle);
-
-    const yAxis = d3.axisLeft(y)
+    svg.call(tickBucketsTitle);
+    const tickBucketsAxis = d3.axisLeft(tickBucketsScale)
         .tickFormat((d, i) => `${yLabels[i]} ms`)
     svg.append("g")
         .attr("transform", translate(margin.left, 0))
         .attr("id", "y-axis")
         .attr("class", "axis")
-        .call(yAxis);
+        .call(tickBucketsAxis);
 
 
     // Drawing the Heatmap
@@ -102,13 +99,63 @@ const drawHeatmap = (d3Container, perfData) => {
         .enter()
         .append('rect')
         .filter(d => typeof d.freq == 'number')
-        .attr('x', (d, i) => x(d.x))
-        .attr('y', (d, i) => y(d.y))
-        .attr('width', x.bandwidth())
-        .attr('height', y.bandwidth())
+        .attr('x', (d, i) => timeScale(d.x))
+        .attr('y', (d, i) => tickBucketsScale(d.y))
+        .attr('width', timeScale.bandwidth())
+        .attr('height', tickBucketsScale.bandwidth())
         .attr('fill', d => color(d.freq))
         .attr('stroke', d => color(d.freq))
 
+
+    // Y2 Axis - Player count
+    const y2Padding = Math.round(tickBucketsScale.bandwidth() / 2)
+    const clientsScale = d3.scaleLinear()
+        .domain([0, d3.max(snapClients.map(t => t.c))])
+        .range([height - margin.bottom - y2Padding, margin.top + y2Padding])
+
+    const clientsTitle = svg.append('g')
+        .attr("transform", 'rotate(-90) ' + translate(0 - (height / 2), width - 25))
+        .attr("class", "axisTitle")
+    clientsTitle.append('text')
+        .style("text-anchor", "middle")
+        .attr("dy", "0.5em")
+        .text("Players")
+    clientsTitle.append('rect')
+        .attr("x", '-2.75em')
+        .attr("y", "-0.25em")
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill", 'gold')
+        .attr("stroke", "dimgrey")
+        .attr("stroke-width", 2)
+
+    const clientsAxis = d3.axisRight(clientsScale)
+    svg.append("g")
+        .attr("transform", translate(width - margin.right, 0))
+        .attr("id", "y2-axis")
+        .attr("class", "axis")
+        .call(clientsAxis);
+
+    const clientsLine = d3.line()
+        .defined(d => !isNaN(d.c))
+        .x(d => timeScale(d.x))
+        .y(d => clientsScale(d.c))
+    svg.append("path")
+        .datum(snapClients)
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 6)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("d", clientsLine);
+    svg.append("path")
+        .datum(snapClients)
+        .attr("fill", "none")
+        .attr("stroke", "gold")
+        .attr("stroke-width", 2)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("d", clientsLine);
 
 
     d3Container.append(svg.node());
